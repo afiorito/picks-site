6c92fa968d55318ef8e08f80e35dcb4284020ffa
Anthony Fiorito <14360690+afiorito@users.noreply.github.com>
2020-10-14 11:25:21 -0500

    fix: adjust favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,10 +26,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Picks`,
+        short_name: `Picks`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#fff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
