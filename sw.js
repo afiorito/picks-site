9933de1ed6696fcbceabf605734dcd763e5e7557
Anthony Fiorito <14360690+afiorito@users.noreply.github.com>
2020-10-14 13:52:19 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2d1e41fc686989850314.js"
+    "url": "webpack-runtime-a9a722b8054f15808f5d.js"
   },
   {
-    "url": "styles.3dcb2f5d2ea4b7fb388d.css"
+    "url": "styles.536a4f7f8cb83f314f77.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a0406dbbf044fb2cfc2d462508a39d9c"
+    "revision": "ba8b77b1f7040cae62b8d9d6264853ab"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
