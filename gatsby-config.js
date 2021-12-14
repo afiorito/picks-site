f47ac46b1721e3ab9433220f50c5779fb51c5d48
Anthony Fiorito <14360690+afiorito@users.noreply.github.com>
2021-12-14 17:36:56 -0600

    fix: resolve markdown image hyperlinking

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
+              linkImagesToOriginal: false,
               maxWidth: 1100,
             },
           },
